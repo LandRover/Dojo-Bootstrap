33dcaea4e56b0997bde16acbf8fbcc90d40ae8f6
Oleg <github.com@landroverz.com>
2014-11-02 09:27:36 +0200

    added async prop

--- dojo_config.js
+++ dojo_config.js
@@ -1,9 +1,9 @@
 var dojoConfig = {
   baseUrl: './',
+  async: true,
   packages: [
     {name: 'dcl', location: 'vendors/dcl', main: 'dcl'},
     {name: 'dojo', location: 'vendors/dojo'},
     {name: 'lodash', location: 'vendors/lodash/dist', main: 'lodash'}
-  ],
-  async: true
+  ]
 };
