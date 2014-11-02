2ccdec0e098e81c701e5ce04d60f6847e32fe51e
root <root@Paprika.coddex.com>
2014-11-02 09:25:34 +0200

    vendors list added

--- dojo_config.js
+++ dojo_config.js
@@ -1,2 +1,9 @@
 var dojoConfig = {
+  baseUrl: './',
+  packages: [
+    {name: 'dcl', location: 'vendors/dcl', main: 'dcl'},
+    {name: 'dojo', location: 'vendors/dojo'},
+    {name: 'lodash', location: 'vendors/lodash/dist', main: 'lodash'}
+  ],
+  async: true
 };
