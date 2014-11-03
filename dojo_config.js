af63c770372679ef2f17f8b44f667d1d1cabe86a
OG <github.com@landroverz.com>
2014-11-03 11:21:32 +0200

    Delete dojo_config.js

--- dojo_config.js
+++
@@ -1,9 +0,0 @@
-var dojoConfig = {
-  baseUrl: './',
-  async: true,
-  packages: [
-    {name: 'dcl', location: 'vendors/dcl', main: 'dcl'},
-    {name: 'dojo', location: 'vendors/dojo'},
-    {name: 'lodash', location: 'vendors/lodash/dist', main: 'lodash'}
-  ]
-};
