bc9890bff771bb2e02970c5a97813895f676a7e9
Oleg <github.com@landroverz.com>
2014-11-04 20:02:40 +0200

    changed features list

--- dojo.profile.js
+++ dojo.profile.js
@@ -1,67 +1,91 @@
-var profile = (function() {
-    "use strict";
-    
-    var excludeList = {
-        "package": 1
-    };
-    
-    return {
-        basePath: "./",
-        releaseDir: ".build", // build tmp dir
-        action: "release",
-        layerOptimization: "closure",
-        mini: true,
-        selectorEngine: "acme",
-        
-        dojoBootText: "(function(){ require({cache:{}}); require.boot && require.apply(null, require.boot); })();",
-        
-        defaultConfig: {
-            async: 1,
-            hasCache: {
-                "config-selectorEngine": "lite",
-                "dojo-built": 1,
-                "dojo-loader": 1,
-                "dom": 1,
-                "host-browser": 1
-            }
-        },
-        
-        staticHasFeatures: {
-            "config-dojo-loader-catches": 0,
-            "config-stripStrict": 0,
-            "config-tlmSiblingOfDojo": 0,
-            "dojo-cdn": 0,
-            "dojo-config-api": 0,
-            "dojo-loader-eval-hint-url": 1,
-            "dojo-log-api": 0,
-            "dojo-sniff": 0,
-            "dojo-sync-loader": 0,
-            "dojo-timeout-api": 0,
-            "ie-event-behavior": 0
-        },
- 
-        layers: {
-            "dojo/dojo": {
-                include: [],
-                customBase: true
-            },
-            "dist/bootstrap": {
-                include: [
-                    'bootstrap'
-                ],
-                customBase: true,
-                boot: true
-            }
-        },
-        
-        resourceTags: {
-            amd: function(filename) {
-                return (/\.js$/).test(filename);
-            },
-            
-            exclude: function(filename, md) {
-                return md in excludeList;
-            }
-        }
-    };
+var profile = (function(){
+
+  'use strict';
+
+  var miniExcludes = {
+    'package': 1
+  };
+
+  return {
+    basePath: "./",
+    releaseDir: ".build",
+    action: "release",
+    layerOptimize: "closure",
+    optimize: "closure",
+    cssOptimize: "comments", 
+    mini: true,
+    selectorEngine: "acme",
+    stripConsole: "all",
+
+    dojoBootText: "(function(){ require({cache:{}}); require.boot && require.apply(null, require.boot); })();",
+
+    defaultConfig: {
+      hasCache:{
+        "dojo-built": 1,
+        "dojo-loader": 1,
+        "dom": 1,
+        "host-browser": 1,
+        "config-selectorEngine": "acme"
+      },
+      async: 1
+    },
+
+    staticHasFeatures: {
+      "config-deferredInstrumentation": 0,
+      "config-dojo-loader-catches": 0,
+      "config-tlmSiblingOfDojo": 0,
+      "csp-restrictions": 1,
+      "dojo-amd-factory-scan": 0,
+      "dojo-cdn": 0,
+      "dojo-combo-api": 0,
+      "dojo-config-api": 1,
+      "dojo-config-require": 0,
+      "dojo-debug-messages": 0,
+      "dojo-dom-ready-api": 1,
+      "dojo-firebug": 0,
+      "dojo-guarantee-console": 0,
+      "dojo-has-api": 1,
+      "dojo-inject-api": 1,
+      "dojo-loader": 1,
+      "dojo-log-api": 0,
+      "dojo-modulePaths": 0,
+      "dojo-moduleUrl": 0,
+      "dojo-publish-privates": 0,
+      "dojo-requirejs-api": 0,
+      "dojo-sniff": 0,
+      "dojo-sync-loader": 0,
+      "dojo-test-sniff": 0,
+      "dojo-timeout-api": 0,
+      "dojo-trace-api": 0,
+      "dojo-undef-api": 0,
+      "dojo-v1x-i18n-Api": 1,
+      "dom-addeventlistener": 1,
+      "dom": 1,
+      "extend-dojo": 1,
+      "host-browser": 1
+    },
+
+    layers: {
+      "dojo/dojo": {
+        include: [],
+        customBase: true
+      },
+      "release/bootstrap": {
+        include: [
+          "bootstrap"
+        ],
+        customBase: true,
+        boot: true
+      }
+    },
+
+    resourceTags: {
+      miniExclude: function(filename, mid){
+        return mid in miniExcludes;
+      },
+      amd: function(filename, mid) {
+        return (/\.js$/).test(filename);
+      }
+    }
+  };
 })();
